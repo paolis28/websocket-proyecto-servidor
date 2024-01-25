5499cea25e1b839b583353648e07f9e31c63b8ca
paolis28 <shinge@outlook.es>
2024-01-25 12:09:56 -0600

    cambios en el archivo env

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-const app = require('./app.js');
-const sequelize = require('sequelize');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const appExpress = express();
+const server = http.createServer(appExpress);
+const io = socketIo(server);
+const cors = require('cors');
 
-async function main(){
-    try{
-        // await sequelize.sync()
-        app.listen(3000)
-        console.log("Server is listen on port", 3000)
-    }catch(error){
-        console.log("Fallo la conexion a la base de datos", error)
-    }
-}
+// Habilita CORS
+appExpress.use(cors());
 
-main();
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.on('chat message', (message) => {
+        console.log('Mensaje recibido:', message);
+        io.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
